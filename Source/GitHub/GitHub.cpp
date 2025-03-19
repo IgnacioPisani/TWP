f5b988ce725b49ce25495c4056e40b35980a3b02
Ignacio Pisani <69583470+IgnacioPisani@users.noreply.github.com>
2025-03-19 18:05:07 -0300

    Creacion Proyecto
    Inicio Proyecto

---
+++ Source/GitHub/GitHub.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitHub.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitHub, "GitHub" );
